cfcdfbeab079ee35ce1ad2d53676ce2a84afceb8
jmoreno1852 <javiermmvv02@gmail.com>
2025-02-08 20:34:05 +0100

    receive data implementation

--- src/server.py
+++ src/server.py
@@ -6,13 +6,15 @@ def tcp_server(host,port):
     s.bind((host,port))
     s.listen(2)
     print(f"TCP Server up at {host}:{port}")
+
+#I need to make a function just to receive connections
+#Do i need a thread running on this funct?
+#How do i manage data then
+def listener(s):
     while True:
         c, c_addr = s.accept()
         data = c.recv(1024)
-
-#I need to make a function just to receive connections
-#
-
+        
 def ls():
     return(os.listdir(path='.'))
 
@@ -23,8 +25,42 @@ def cat(file):
         return read_data
     except OSError:
         print("Error while openning file")
-def get():
-    pass
+def get(file):
+    with open(file,"r") as f:
+        return f.read()
 
-def put():
-    pass
+def put(file, f_data): #Should this be on client??
+    with open(file,"w") as f:
+        f.write(f_data)
+
+def handle_client(s):
+    while True:
+        c, addr = s.accept()
+        print(f"Connection from {addr}")
+        thread = treading.Thread(target=receive_data,args=(c,addr))
+
+def receive_data(c,addr):
+    while True:
+        try:
+            data = c.recv(1024).decode('utf-8')
+            command, file = data.split(" ",1)
+
+            if command == 'get':
+                f_data = get(file)
+                c.sendall(data.encode('utf-8'))
+            elif command == 'put':#Needs implementation on client side
+                put(file,f_data)
+            elif command == 'cat':
+                f_data = cat(file)
+                c.sendall(data.encode('utf-8'))
+            elif command == 'ls':
+                f_data = ls()
+                c.sendall(data.encode('utf-8'))
+            else:
+                print("Useful commands: [ get || put || cat || ls]")
+            
+        except:
+            print("Error at receive_data")
+            c.close()
+
+    c.close()
